cd082a8e6a2458675a923d32bff3da29ce084d9c
Michail Chatzimanolakis <michaich@eu-login-14.euler.ethz.ch>
2022-11-23 11:04:23 +0100

    removed tabs from source files for tutorial on mpi

--- tutorials/tutorial07_MPI/skeleton-code/monte-carlo.cpp
+++ tutorials/tutorial07_MPI/skeleton-code/monte-carlo.cpp
@@ -4,39 +4,39 @@
 
 double F(double x, double y) // Integrand
 {
-  	if (x * x + y * y < 1.) return 4.; // inside unit circle 
-  	return 0.;
+      if (x * x + y * y < 1.) return 4.; // inside unit circle 
+      return 0.;
 }
 
 double C0(int n, int seed = 0)
 {
-	double sum = 0.;
+    double sum = 0.;
 
-	std::default_random_engine g(seed);  // random generator with seed 0
+    std::default_random_engine g(seed);  // random generator with seed 0
 
-	for (int i = 0; i < n; ++i)
-	{
-		std::uniform_real_distribution<double> u; // uniform distribution in [0, 1]
-		double x = u(g);
-		double y = u(g);
-		sum += F(x, y);
-	}
-	return sum/n;
+    for (int i = 0; i < n; ++i)
+    {
+        std::uniform_real_distribution<double> u; // uniform distribution in [0, 1]
+        double x = u(g);
+        double y = u(g);
+        sum += F(x, y);
+    }
+    return sum/n;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int n = (argc > 1 ? atoi(argv[1]) : 1e8); // number of samples (default value is 10^8)
+    int n = (argc > 1 ? atoi(argv[1]) : 1e8); // number of samples (default value is 10^8)
 
-	double ref = 3.14159265358979323846; // reference solution
+    double ref = 3.14159265358979323846; // reference solution
 
-	double t0 = MPI_Wtime();
+    double t0 = MPI_Wtime();
 
-	double res = C0(n,0);
+    double res = C0(n,0);
 
-	double t1 = MPI_Wtime();
+    double t1 = MPI_Wtime();
 
-   	std::cout << "res = " << res << " error = " << res-ref << " time = " << t1 - t0 << "\n";
+    std::cout << "res = " << res << " error = " << res-ref << " time = " << t1 - t0 << "\n";
 
-	return 0;
+    return 0;
 }
--- tutorials/tutorial07_MPI/skeleton-code/quadratic.cpp
+++ tutorials/tutorial07_MPI/skeleton-code/quadratic.cpp
@@ -5,35 +5,35 @@
 
 int main(int argc, char** argv)
 {
-	const int n = 32768;
+    const int n = 32768;
 
-	double * const A = new double[n*n];
-	double * const v = new double[n];
-	double * const w = new double[n];
+    double * const A = new double[n*n];
+    double * const v = new double[n];
+    double * const w = new double[n];
 
-	// initialize A_ij = (i + 2*j) / n^2
-	// initialize v_i  =  1 + 2 / (i+0.5)
-	// initialize w_i  =  1 - i / (3*n)
-	const double fac0 = 1./(n*n);
-	const double fac1 = 1./(3.*n);
-	for (int i=0; i<n; ++i)
-	{
-        v[i] = 1. + 2. / (i + 0.5);
-        w[i] = 1. - i * fac1;
-        for (int j=0; j<n; ++j)
-           	A[i*n + j] = (i + 2.*j) * fac0;
+    // initialize A_ij = (i + 2*j) / n^2
+    // initialize v_i  =  1 + 2 / (i+0.5)
+    // initialize w_i  =  1 - i / (3*n)
+    const double fac0 = 1./(n*n);
+    const double fac1 = 1./(3.*n);
+    for (int i=0; i<n; ++i)
+    {
+         v[i] = 1. + 2. / (i + 0.5);
+         w[i] = 1. - i * fac1;
+         for (int j=0; j<n; ++j)
+              A[i*n + j] = (i + 2.*j) * fac0;
     }
 
-	double myresult = 0.;
+    double myresult = 0.;
     for (int i=0; i<n; ++i)
     for (int j=0; j<n; ++j)
-		myresult += v[i] * A[i*n + j] * w[j];
+         myresult += v[i] * A[i*n + j] * w[j];
 
-	std::cout << "Result = " << myresult << std::endl;
+    std::cout << "Result = " << myresult << std::endl;
 
-	delete[] A;
-	delete[] v;
-	delete[] w;
+    delete[] A;
+    delete[] v;
+    delete[] w;
 
-	return 0;
+    return 0;
 }
