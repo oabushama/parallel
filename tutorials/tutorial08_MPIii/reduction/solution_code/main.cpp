cbeaa36c5fc0eec34164fa613dc06ad8eceb6869
Athena Economides <eceva@ethz.ch>
2022-11-30 18:04:46 +0100

    tutorial MPI: bugfix reduction/solution_code

--- tutorials/tutorial08_MPIii/reduction/solution_code/main.cpp
+++ tutorials/tutorial08_MPIii/reduction/solution_code/main.cpp
@@ -3,20 +3,23 @@
 #include <random>
 #include <cmath>
 #include <mpi.h>
+#include <assert.h>
 
 inline long exact(const long N){
     return N*(N+1)/2;
 }
 
-void reduce_mpi(const int rank, long& mysum){
-    double sum;
+long reduce_mpi(const int rank, long mysum){
+    long sum;
+    /* int MPI_Reduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype,
+               MPI_Op op, int root, MPI_Comm comm) */
     MPI_Reduce(&mysum, &sum, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD); //collect partial results from each rank to rank 0.
+    return sum;
 }
 
-void reduce_manual(int rank, int size, long& sum){
+long reduce_manual(int rank, int size, long sum){
     // Size is a power of 2 for simplicity
     const int TAG = 1337;
-
     for(int send_rec_border = size/2; send_rec_border >= 1; send_rec_border/=2)
     {
         if(rank < send_rec_border)
@@ -24,37 +27,31 @@ void reduce_manual(int rank, int size, long& sum){
             long buffer;
             MPI_Recv(&buffer, 1, MPI_LONG, rank+send_rec_border, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             sum += buffer;
-        } else if(rank <= send_rec_border*2){
+        } else if(rank <= send_rec_border*2)
+        {
             MPI_Send(&sum, 1, MPI_LONG, rank-send_rec_border, TAG, MPI_COMM_WORLD);
         }
     }
+    return sum;
 }
 
 
 int main(int argc, char** argv){
     const long N = 1000000;
 
-    // Initialize MPI
-    MPI_Init(&argc, &argv);
 
+    //-------------------------
+    // Initialize MPI
+    //-------------------------
+    MPI_Init(&argc, &argv);
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 
-    // -------------------------
-    // Case 1: naive reduction
-    // -------------------------
-/*
-    long sum = 0;
-    reduce_mpi(rank, sum);
-*/
-
- 
-    // -------------------------
-    // Case 2: distributed reduction
-    // -------------------------
-
+    //-------------------------
+    // Partial sums
+    //-------------------------
     // Determine work load per rank
     long N_per_rank = N / size;
 
@@ -66,26 +63,32 @@ int main(int argc, char** argv){
         N_end += N % size;
     }
 
-    // Compute partial sum: N_start + (N_start+1) + ... + (N_start+N_per_rank-1)
+    // Compute partial in each rank: N_start + (N_start+1) + ... + (N_start+N_per_rank-1)
     long sum = 0;
     for(long i = N_start; i <= N_end; ++i){
         sum += i;
     }
 
-    // Reduction
-    reduce_manual(rank, size, sum);
 
- 
-    // -------------------------
-    // Print the result
-    // -------------------------
+    //-------------------------
+    // Reduction
+    //-------------------------
+
+    // Case 1: naive reduction
+    long sum_mpi_v1 = reduce_mpi(rank, sum);
     if(rank == 0){
-        std::cout << std::left << std::setw(25) << "Final result (exact): " << exact(N) << std::endl;
-        std::cout << std::left << std::setw(25) << "Final result (MPI): " << sum << std::endl;
+        std::cout << std::left << std::setw(30) << "Final result (exact): " << exact(N) << std::endl;
+        std::cout << std::left << std::setw(30) << "Final result (MPI:naive): " << sum_mpi_v1 << std::endl;
     }
 
-    // Finalize MPI
+    // Case 2: distributed reduction
+    long sum_mpi_v2 = reduce_manual(rank, size, sum);
+    if(rank == 0){
+        std::cout << std::left << std::setw(30) << "Final result (MPI:manual): " << sum_mpi_v2 << std::endl;
+    }
+
+
     MPI_Finalize();
-    
+
     return 0;
 }
